3617752e773fdf50ceba1726a9c9788834e971c4
Anthony Scola <anthonyscola@outlook.com>
2023-11-19 19:47:08 -0500

    Fix validation logic for type Number

--- src/components/AutoDialog/helpers.tsx
+++ src/components/AutoDialog/helpers.tsx
@@ -5,6 +5,7 @@ export const validateControl = (Control: Control, value: FormDataTypes, errorsAr
   if(Control.controlProps.required === false){
     return;
   }
+
   if(value === null) value = '';
   const controlId = Control.controlProps.id;
   if(typeof(value) === 'string'){
@@ -15,6 +16,7 @@ export const validateControl = (Control: Control, value: FormDataTypes, errorsAr
       setErrors(errorsArray.filter(item => item !== controlId));
     }
   }
+
   if(typeof(value) === 'number'){
     if('disalowedValues' in Control.controlProps && Control.controlProps.disalowedValues !== undefined){
       if(Control.controlProps.disalowedValues.includes(value) && !errorsArray.includes(controlId)){
@@ -23,19 +25,24 @@ export const validateControl = (Control: Control, value: FormDataTypes, errorsAr
       }
     }
     if('min' in Control.controlProps && Control.controlProps.min !== undefined){
-      if((value < Control.controlProps.min) && !errorsArray.includes(controlId)){
-        setErrors([...errorsArray, controlId]);
+      if((value < Control.controlProps.min)){
+        !errorsArray.includes(controlId) && setErrors([...errorsArray, controlId]);
+        return;
+      }
+      if(errorsArray.includes(controlId)){
+        setErrors(errorsArray.filter(error => error !== controlId));
         return;
       }
     }
     if ('max' in Control.controlProps && Control.controlProps.max !== undefined){
-        if((value > Control.controlProps.max) && !errorsArray.includes(controlId)){
-          setErrors([...errorsArray, controlId]);
-          return;
+      if((value > Control.controlProps.max) && !errorsArray.includes(controlId)){
+        !errorsArray.includes(controlId) && setErrors([...errorsArray, controlId]);
+        return;
       }
-    }
-    if ((value) && errorsArray.includes(controlId)){
-      setErrors(errorsArray.filter(item => item !== controlId));
+      if(errorsArray.includes(controlId)){
+        setErrors(errorsArray.filter(error => error !== controlId));
+        return;
+      }
     }
   }
 };
